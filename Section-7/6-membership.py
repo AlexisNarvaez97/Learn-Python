abf6cce4e29617b9caaf0f52c3b177691737e091
AlexisNarvaez <alexisnarvaez97@hotmail.com>
2022-05-15 14:30:18 -0500

    4 commit

---
+++ Section-7/6-membership.py
@@ -0,0 +1,30 @@
+x = ["Python", "Java", "Typescript", "Rust"]
+
+
+# Comprar si un valo pertenece a esa variable / objeto
+
+a = "python" in x # False
+a = "Python" in x # True
+
+a = "Typescrip" in x # False
+a = "Typescript" in x # True
+
+a = "python" not in x # True
+a = "Python" not in x # False
+
+a = "Typescrip" not in x # True
+a = "Typescript" not in x # False
+
+
+
+
+
+# String
+
+x = "Alexis"
+
+a = "a" in x # False
+a = "xis" in x # True
+
+
+print("Final")
